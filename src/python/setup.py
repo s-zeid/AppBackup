a37efbe28fc28500fa4bfc28a95cac4b9e69c166
Scott Zeid <s@zeid.me>
2014-12-19 06:46:21 -0600

    [src/python] Add a setup.py file

---
+++ src/python/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2
+
+# This file is public domain via CC0:
+# <https://creativecommons.org/publicdomain/zero/1.0/>
+
+from setuptools import setup, find_packages
+
+setup(
+ name="iosappbackup",
+ version="3.0.dev1",
+ description="Backs up and restores the saved data and settings of"
+             " installed iOS App Store apps.",
+ url="http://code.s.zeid.me/appbackup",
+ author="Scott Zeid",
+ author_email="s@zeid.me",
+ license="X11 License:  https://tldrlegal.com/license/x11-license",
+ classifiers=[
+  "Development Status :: 3 - Alpha",
+  "Environment :: Console",
+  "Intended Audience :: Developers",
+  "Intended Audience :: System Administrators",
+  "Natural Language :: English",
+  "Operating System :: iOS",
+  "Operating System :: MacOS :: MacOS X",
+  "Operating System :: POSIX",
+  "Operating System :: POSIX :: Linux",
+  "Programming Language :: Python :: 2",
+  "Programming Language :: Python :: 2.5",
+  "Programming Language :: Python :: 2 :: Only",
+  "Topic :: Software Development :: Libraries",
+  "Topic :: Software Development :: Libraries :: Python Modules",
+  "Topic :: System :: Systems Administration",
+  "Topic :: Utilities",
+ ],
+ packages=find_packages(),
+ install_requires=["argparse", "CFPropertyList", "simplejson"]
+                 +["iosapplist==3.0dev4"],
+ entry_points={
+  "console_scripts": [
+    "iosappbackup=iosappbackup.cli:main"
+  ]
+ },
+)
