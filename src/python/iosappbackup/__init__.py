d2847dfa79ce461722ed4f7f53f6b9abf75f8581
Scott Zeid <s@zeid.me>
2014-12-23 14:49:20 -0600

    [python] increment version number

--- src/python/iosappbackup/__init__.py
+++ src/python/iosappbackup/__init__.py
@@ -33,7 +33,7 @@
 from __future__ import with_statement
 
 __author__  = "Scott Zeid <s@zeid.me>"
-__version__ = "3.0dev5"
+__version__ = "3.0dev6"
 
 codename    = "Maserati"  # git push all maserati
 
