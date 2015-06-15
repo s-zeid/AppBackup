d6652156669aa36531b9910d999246c47ec3c692
Scott Zeid <s@zeid.me>
2015-06-14 22:28:01 -0500

    [python/setup.py] require iosapplist 3.0.dev30

--- src/python/setup.py
+++ src/python/setup.py
@@ -36,7 +36,7 @@ setup(
  ],
  packages=find_packages(),
  install_requires=["argparse", "CFPropertyList", "simplejson"]
-                 +["iosapplist>=3.0dev27"],
+                 +["iosapplist>=3.0dev30"],
  entry_points={
   "console_scripts": [
     "iosappbackup=iosappbackup:main"
